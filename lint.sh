1a737812db54143c1a11e6b682ea1d49a01121b6
John Clyde <175860+johnclyde@users.noreply.github.com>
2025-05-02 20:09:25 -0700

    Delete lint.sh

--- lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Acorn linter/formatter
-
-# 1. Remove trailing whitespace
-find . -name "*.ac" -exec sed -i '' 's/[[:space:]]*$//' {} \;
-
-# 2. Validate file structure
-find . -name "*.ac" -exec grep -nEH '[^[:space:]]$' {} \; | awk '{print "Trailing whitespace fixed in: " $1}'
